48830d7dd7c3f86d52af7ca371b5633791fa4455
John Costanzo <jrock2004@users.noreply.github.com>
2017-09-23 01:20:41 -0400

    Fixing wraps and adding some comments

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -2,32 +2,26 @@
 
 OS=fedora
 
-echo -e "\n Adding RPM Fusion"
-sudo dnf install
-https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm-E
-%fedora).noarch.rpm
-https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm
--E %fedora).noarch.rpm
+echo -e "\n Adding some extenal sources"
 
+# Install RPM fusion
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm-E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Sources for MS
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod\nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo'
 
-sudo sh -c 'echo -e
-"[code]\nname=Visual Studio
-Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc"
-> /etc/yum.repos.d/vscode.repo'
-
-sudo sh -c 'echo -e
-"[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod
-\nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc"
-> /etc/yum.repos.d/dotnetdev.repo'
-
+# Sources for Spotify
 sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
 
+# Sources for Yarn
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 
+# Lets updated so we can use the new sources
 sudo dnf check-update
 
-sudo dnf install \
+sudo dnf -y install \
  autoconf \
  automake \
  bison \
