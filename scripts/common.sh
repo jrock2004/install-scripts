edb2f53c08147f695832002c5ebfa9c0506a6eb3
John Costanzo <jrock2004@users.noreply.github.com>
2017-10-19 23:49:40 -0400

    Cleaning up stuff that is no longer needed

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,34 +3,3 @@
 # Install some extra tools
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 $HOME/.fzf/install
-
-# Setup groups
-sudo usermod -aG docker $USER
-
-# Get Tmux spotify client
-sudo curl https://raw.githubusercontent.com/jrock2004/tmux-spotify/master/tmux-spotify -o /usr/local/bin/tmux-spotify
-
-sudo chmod +x /usr/local/bin/tmux-spotify
-
-# Setting some dconf settings
-
-# Set clock
-gsettings set org.gnome.desktop.interface clock-format 12h
-
-# Touchpad settings
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-
-# Screensaver settings
-gsettings set org.gnome.desktop.screensaver lock-enabled true
-
-# Nautilus settings
-gsettings set org.gnome.nautilus.preferences show-hidden-files true
-
-# File roller Settings
-gsettings set org.gnome.file-roller.file-selector show-hidden true
-
-# Set screenshot settings
-gsettings set org.gnome.gnome-screenshot default-file-type jpg
-
-# Set auto timezone
-gsettings set org.gnome.desktop.datetime automatic-timezone true
