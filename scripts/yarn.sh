47709395ec9634b3f079a34ce49482684f581792
John Costanzo <jrock2004@users.noreply.github.com>
2017-10-19 23:48:46 -0400

    Cleaning up unused apps

--- scripts/yarn.sh
+++ scripts/yarn.sh
@@ -2,31 +2,16 @@
 
 sudo yarn global add \
   all-the-package-names \
-  bower \
-  browser-sync \
   csslint \
   diff-so-fancy \
-  ember-cli \
-  ember-cli-azure-deploy \
   eslint \
-  express-generator \
-  firebase-tools \
-  generator-aspnet \
   git-standup \
-  gulp \
-  handlebars \
-  pug-lint \
   jscs \
   jshint \
   jsonlint \
   jsxhint \
-  less \
-  nodemon \
-  preact-cli \
-  sass \
   tslint \
-  typescript \
-  yo
+  typescript
 
 # Fix permissions on config dir
 sudo chown -R $USER $HOME/.config
