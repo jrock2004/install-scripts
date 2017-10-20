8b61cb5b67e3fad143a8d628c483277f03451d8f
John Costanzo <jrock2004@users.noreply.github.com>
2017-10-19 23:52:24 -0400

    Cleaning up stuff that is no longer needed

--- scripts/ruby.sh
+++ scripts/ruby.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$OS" != "fedora" ]; then
-  echo "Installing some Gems"
-  $HOME/.rbenv/shims/gem install bundler scss_lint neovim
-else
-  gem install bundler scss_lint
-fi
+gem install bundler scss_lint
