01da6c5ef15b2befe6aa551b16086cd3387970a7
John Costanzo <jrock2004@users.noreply.github.com>
2018-01-20 13:19:13 -0500

    Tweaking for Ubuntu on Windows

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -3,19 +3,22 @@
 # Adding some sources
 ##############################
 
-# VS Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
-# NodeJS
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+if [ "$OS" = "microsoft" ]; then
+  # Neovim repo
+  sudo add-apt-repository ppa:neovim-ppa/stable
+else
+  # VS Code
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+fi
 
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 # Installing some apps
 ###############################
@@ -27,10 +30,13 @@ if [ "$OS" = "microsoft" ]; then
     exuberant-ctags \
     gcc \
     mono-devel \
+    neovim \
     ngrep \
     nodejs \
     python-dev \
+    python-pip \
     python3-dev \
+    python3-pip \
     ruby2.3 \
     silversearcher-ag \
     tmux \
@@ -38,6 +44,10 @@ if [ "$OS" = "microsoft" ]; then
     xclip \
     yarn \
     zsh
+   
+  pip install --upgrade pip
+  pip install --user --upgrade neovim
+  pip3 install --user --upgrade neovim
 else
   sudo apt-get -y install \
     cabextract \
