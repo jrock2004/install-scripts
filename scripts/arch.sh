2edf72cd27c510d85fdd9faf7e5f8d3209413ddd
John Costanzo <jrock2004@users.noreply.github.com>
2018-02-04 22:33:32 -0500

    Remove wine for now

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -21,12 +21,6 @@ sudo pacman -Sy \
 
 yaourt -S \
   visual-studio-code-bin \
-  visual-studio-code-insiders \
-  wine-stable
-
-# Install Winetricks
-wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-sudo mv -v winetricks /usr/local/bin
+  visual-studio-code-insiders
 
 source scripts/common.sh
