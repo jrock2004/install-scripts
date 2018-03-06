ef03d62805cae1684b6665590da60e40159e9478
John Costanzo <jrock2004@users.noreply.github.com>
2018-03-06 18:13:56 -0500

    Adjusting some errors

--- scripts/solus.sh
+++ scripts/solus.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Adding some sources
-##############################
-
-if [ "$OS" = "microsoft" ]; then
-  # Neovim repo
-  sudo add-apt-repository ppa:neovim-ppa/stable
-else
-  # VS Code
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-fi
-
-
 # Installing some apps
 ###############################
 
@@ -24,7 +10,6 @@ sudo eopkg -y install \
   gcc \
   mono-devel \
   neovim \
-  #ngrep \
   nodejs \
   obs-studio \
   openshot-qt \
