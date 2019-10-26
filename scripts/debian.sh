70d4e36e886203984d7e1da4c569270e0f7c3896
John Costanzo <jrock2004@gmail.com>
2019-10-26 18:41:08 -0400

    Fixing filename

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -50,7 +50,7 @@ sudo apt-get -y install \
 	zsh
 
 curl -LO https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
-sudo dpkg -i bat_0.12.1_amd.deb
+sudo dpkg -i bat_0.12.1_amd64.deb
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
