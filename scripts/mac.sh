f337dbb08425a05a2b541fb688ab19d86890d16b
John Costanzo <jrock2004@users.noreply.github.com>
2019-10-29 00:13:16 -0400

    Upgrading NVM version

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -66,4 +66,4 @@ done
 mkdir -p ~/.vim/files/info
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
